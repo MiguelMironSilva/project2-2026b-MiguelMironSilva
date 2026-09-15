7e3189e09c38e06300a09cd2166263f520d9c7f0
MiguelMironSilva <miguelmironsilva@gmail.com>
2026-09-14 23:16:19 -0300

    Configurações dos routers adicionados

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+from app.routers import movies
+
 
 app = FastAPI(
     title="Webapp Filmes API",
     version="0.1.0",
 )
+
+app.include_router(movies.router)
 
 
 @app.get("/")
